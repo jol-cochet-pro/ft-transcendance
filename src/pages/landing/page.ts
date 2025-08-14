de14ca333528130c88d33e030b3f65f705b5175a
jcochet <jcochet@student.42angouleme.fr>
2025-08-14 13:16:04 +0200

    fix: delete user profile button

--- src/pages/landing/page.ts
+++ src/pages/landing/page.ts
@@ -1,13 +1,11 @@
 import Component from '../../react/component'
 import Navbar from '../../components/navbar'
-import React from '../../react/react';
 
 export default class LandingPage extends Component {
     public render(): string {
         return `
             <div class="flex flex-col gap-4">
                 ${new Navbar()}
-                <button onClick=${this.register(() => React.navigate("/user-profile"))}> user profle </button>
             </div>
         `;
     }
