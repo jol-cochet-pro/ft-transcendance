b493f12cd518c313c76f2daae547b023b1647c94
jcochet <jcochet@student.42angouleme.fr>
2025-08-14 15:41:08 +0200

    feat (in progress): games part

---
+++ src/pages/landing/games.ts
@@ -0,0 +1,10 @@
+import Component from "../../react/component";
+
+
+export default class Games extends Component
+{
+    render(): string {
+        throw new Error("Method not implemented.");
+    }
+    
+}
