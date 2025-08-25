d549f162f37b02358ae164cf5bfe67a6d8f0146e
jcochet <jcochet@student.42angouleme.fr>
2025-08-25 15:36:01 +0200

    feat: update navbar

--- frontend/src/components/navbar.ts
+++ frontend/src/components/navbar.ts
@@ -19,6 +19,7 @@ export default class Navbar extends Component {
     }
 
     public render(): string {
+        const formatter = Intl.NumberFormat("fr-FR");
         return `
             <nav class="flex justify-between items-center w-full p-3">
                 <img src="/logo.png" alt="logo" width=70 onClick=${this.register(() => this.onLogoClick())} />
@@ -29,13 +30,16 @@ export default class Navbar extends Component {
                     <button class="text-white transition hover:scale-110" onClick=${this.register(() => this.onClick('testimonials'))}> Avis </button>
                     <button class="text-white transition hover:scale-110" onClick=${this.register(() => this.onClick('partners'))}> Partenaires </button>
                 </div>
-                <div class="flex gap-3" onClick=${this.register(() => this.onProfileClick())}>
+                <button class="flex gap-3 transition-colors duration-200 hover:bg-surface rounded-lg p-2" onClick=${this.register(() => this.onProfileClick())}>
                     <div class="flex flex-col">
-                        <span class="text-white text-xs font-semibold"> Jolan Cochet </span>
-                        <span class="text-white text-xs font-semibold opacity-70"> 154121 € </span>
+                        <h3> Jolan Cochet </h3>
+                        <h3 class="flex items-center gap-2 opacity-70">                    
+                            <span> ${formatter.format(154214)} </span>
+                            <i data-lucide="banknote" class="w-6 h-6 stroke-primary"></i>
+                        </h3>
                     </div>
-                    <img src="/pp.png" alt="profile picture" width=30 />
-                </div>
+                    <img src="/pp.png" alt="profile picture" class="w-12 aspect-square" />
+                </button>
             </nav>
         `
     }
